9982c17a9f7c3a7d9fcb9c1c80cfcb2361d7d162
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-15 13:31:06 -0300

    Todas dependecias do projeto foram linkadas

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
+<?php
+
+    include_once("config/url.php");
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Agenda de contatos</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.min.js">
+
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?php $_BASE_URL ?>css/styles.css">
 </head>
 <body>
+
+    <h1>Testando agenda</h1>
+    <i class="fas fa-eye"></i>
+
+
     
 </body>
 </html>
