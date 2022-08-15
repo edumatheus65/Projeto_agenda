22467273c7969c1f62719d9ca5a0ebac06ff3d57
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-15 13:50:36 -0300

    Iniciado a separacao dos templates, foi criado

---
+++ Templates/header.php
@@ -0,0 +1,25 @@
+<?php
+
+    include_once("config/url.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de contatos</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.min.js">
+
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?php $_BASE_URL ?>css/styles.css">
+</head>
+<body>
