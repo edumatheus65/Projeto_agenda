8ae9f93d061b24326cca3ad60618767ae23897bf
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-16 08:15:34 -0300

    Criado a nav do agenda eletronica com dois links agenda e criar contatos

--- Templates/header.php
+++ Templates/header.php
@@ -14,20 +14,26 @@
     <title>Agenda de contatos</title>
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous"/>
 
     <!-- Font-Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 
     <!-- CSS -->
-    <link rel="stylesheet" href="<?php $_BASE_URL ?>css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
 </head>
 <body>
-
-    <nav>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
-                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
-            </a>
-        </nav>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+        <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
+      </a>
+      <div>
+        <div class="navbar-nav">
+          <a class="nav-link active" id="home-link" href="<?= $BASE_URL ?>index.php">Agenda</a>
+          <a class="nav-link active" href="<?= $BASE_URL ?>create.php">Adicionar Contato</a>
+        </div>
+      </div>
     </nav>
+  </header>
+    
