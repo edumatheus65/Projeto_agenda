181a7fbec2921a4009b8cde6f25acbee4ebc6ead
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-17 10:52:28 -0300

    Criado a query com o metedo prepare, resgatando dados com bind

--- Config/process.php
+++ Config/process.php
@@ -10,23 +10,42 @@ include_once("connection.php");
 //Para eu poder saber qual a minha base URL aqui nesse cara
 include_once("url.php");
 
-$contacts = [];
 
+$id;
 
+if(!empty($_GET)) {
+  $id = $_GET["id"];
+}
 //Retorna os dados de um contato
+if(!empty($id)) {
+
+    $query = "SELECT * FROM contacts WHERE id = :id";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":id", $id);
+
+    $stmt->execute();
+
+    $contact = $stmt->fetch();
+
+} else {
+
+    //Retorna todos os contatos
+    $contacts = [];
+    
+    //Primeira coisa a fazer é puxar os dados da query
+    $query = "SELECT * FROM contacts";
+    //Como eu não tenho paramentros eu não tenho bind_paran aqui nesse caso
+    $stmt = $conn->prepare($query);
+
+    $stmt->execute();
+
+    //Não esquecer de inserir o processamento no Header, pois toda vez que eu iniciar uma página eu vou ter o processamento do meu process.php
+    $contacts = $stmt->fetchAll();
+}
 
 
 
 
 
-
-//Retorna todos os contatos
-//Primeira coisa a fazer é puxar os dados da query
-$query = "SELECT * FROM contacts";
-//Como eu não tenho paramentros eu não tenho bind_paran aqui nesse caso
-$stmt = $conn->prepare($query);
-
-$stmt->execute();
-
-//Não esquecer de inserir o processamento no Header, pois toda vez que eu iniciar uma página eu vou ter o processamento do meu process.php
-$contacts = $stmt->fetchAll();
--- show.php
+++ show.php
@@ -5,7 +5,11 @@ include_once("templates/header.php");
 ?>
 
 <div class="container" id="view-contact-container">
-    <h1 id="main-title"><?php $contact['name'] ?></h1>
+    <h1 id="main-title"><?= $contact['name'] ?></h1>
+    <p class="bold">Telefone: </p>
+    <p><?= $contact['phone'] ?></p>
+    <p class="bold">Observações: </p>
+    <p><?= $contact['observations'] ?></p>
 </div>
 
 <?php
