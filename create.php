a47f375294129db2ac00ca57bfc77b9433426428
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-18 09:09:23 -0300

    finalizado a parte de creacao e estilo css bootstrap da view creat

--- create.php
+++ create.php
@@ -5,7 +5,7 @@
     <div class="container">
       <?php include_once("templates/backbtn.html"); ?>
       <h1 id="main-title">Criar contato</h1>
-      <form action="<?= $BASE_URL ?>config/process.php" method="POST">
+      <form id="creat-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
 
         <input type="hidden" name="type" value="create">
         <div class="form-group">
