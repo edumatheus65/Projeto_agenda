3375afa5ed30f07a413112b94f7f9876dd9330c4
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-18 09:41:54 -0300

    Setup do execute referente a criacao de um novo contato no backend

--- Config/process.php
+++ Config/process.php
@@ -10,8 +10,49 @@ include_once("connection.php");
 //Para eu poder saber qual a minha base URL aqui nesse cara
 include_once("url.php");
 
+$data = $_POST;
 
-$id;
+// MODIFICAÇÕES NO BANCO
+if(!empty($data)) {
+
+    print_r($data);
+
+    //Criar Contato
+    if($data["type"] === "create") {
+        
+        $name = $data["name"];
+        $phone = $data["phone"];
+        $observations = $data["observations"];
+
+        $query = "INSERT INTO contacts (name, phone, observations), VALUES (:name :phone :observations)";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":name", $name);
+        $stmt->bindParam(":phone", $phone);
+        $stmt->bindParam(":observations", $observations);
+
+        try {
+            
+            $stmt->execute();
+            $_SESSION["msg"] = "Contato criado com sucesso!!";            
+
+
+
+        } catch (PDOException $e) {
+            //ERRO DE CONEXÃO
+            $error = $e->getMessage();
+            echo "Erro: $error"; 
+        }
+
+
+    }
+
+
+// SELEÇÃO DE DADOS
+} else {
+
+    $id;
 
 if(!empty($_GET)) {
   $id = $_GET["id"];
@@ -44,6 +85,16 @@ if(!empty($id)) {
     //Não esquecer de inserir o processamento no Header, pois toda vez que eu iniciar uma página eu vou ter o processamento do meu process.php
     $contacts = $stmt->fetchAll();
 }
+
+}
+
+// ENCERRAR CONEXÃO
+// NÃO É O MODO MAIS ELEGANTE, PORÉM FUNCIONA
+
+$conn = null;
+
+
+
 
 
 
