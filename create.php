ebf60683eddfbbd572b739fec7a330a889537db3
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-18 08:41:28 -0300

    Criado input para cadastro do nome e um buttom submit para envio de formulario

--- create.php
+++ create.php
@@ -5,6 +5,15 @@
     <div class="container">
       <?php include_once("templates/backbtn.html"); ?>
       <h1 id="main-title">Criar contato</h1>
+      <form action="<?= $BASE_URL ?>config/process.php" method="POST">
+
+        <input type="hidden" name="type" value="create">
+        <div class="form-group">
+            <label for="name">Nome do contato</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
     </div>
     
 
